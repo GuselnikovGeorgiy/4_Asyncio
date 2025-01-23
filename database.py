6d0f3c1ba43acb9b4e92fb89f3c23cfd3f6dd803
Jorique <guselnikovgeorgiy@gmail.com>
2025-01-24 01:16:28 +0300

    fixed db

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
-from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from config import DB_HOST, DB_NAME_ASYNC, DB_PASS, DB_PORT, DB_USER
 
-
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME_ASYNC}"
 Base = declarative_base()
 
 engine = create_async_engine(DATABASE_URL)
@@ -24,4 +23,3 @@ async def save(data):
             except Exception as e:
                 await session.rollback()
                 raise e
-
