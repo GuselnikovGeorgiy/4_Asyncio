fea720db8ec8fabde894414059b71866654491b3
Jorique <guselnikovgeorgiy@gmail.com>
2025-01-23 22:55:14 +0300

    added database.py

---
+++ database.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+Base = declarative_base()
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def save(data):
+    async with async_session_maker() as session:
+        async with session.begin():
+            try:
+                session.add_all(data)
+                await session.commit()
+            except Exception as e:
+                await session.rollback()
+                raise e
+
