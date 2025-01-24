16d2cce5bffceb2fa16556a4e22e14b2fb687a1d
Jorique <guselnikovgeorgiy@gmail.com>
2025-01-24 20:03:18 +0300

    increased pool_size

--- database.py
+++ database.py
@@ -5,21 +5,10 @@ from config import DB_HOST, DB_NAME_ASYNC, DB_PASS, DB_PORT, DB_USER
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME_ASYNC}"
 Base = declarative_base()
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, pool_size=10, max_overflow=20, pool_pre_ping=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-
-async def save(data):
-    async with async_session_maker() as session:
-        async with session.begin():
-            try:
-                session.add_all(data)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
